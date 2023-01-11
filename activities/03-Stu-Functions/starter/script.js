fa1412c3b10fde26ad38a1f30d35cc194322ec66
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-11 19:20:10 +0000

    added class exercises and completed first student exercise

---
+++ activities/03-Stu-Functions/starter/script.js
@@ -0,0 +1,19 @@
+// Write Your Code Below
+function add(a,b){
+    let res = a+b;
+    return res;
+}
+
+const subtract = function(a,b){
+    let res = a-b;
+    return res;
+}
+
+function area(a){
+    let res = a*a;
+    return res
+}
+
+console.log(add(1,2));
+console.log(subtract(20,10));
+console.log(area(4));
